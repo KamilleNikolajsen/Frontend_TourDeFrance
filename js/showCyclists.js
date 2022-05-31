845c142bc47988379cc7d6416def82d2093f80f0
kamillenikolajsen <knikolajsen4@gmail.com>
2022-05-31 05:54:39 +0200

    Add sort functions, html and css

--- js/showCyclists.js
+++ js/showCyclists.js
@@ -1,9 +1,18 @@
 "use strict";
 
+const sortButton = document.querySelector("#sortButton");
+const randomButton = document.querySelector("#randomButton");
+const timeButton = document.querySelector("#timeButton");
+const cyclistsSortedUrl = "http://localhost:8080/ShowCyclistsSortedByTeam"
 const cyclistUrl = "http://localhost:8080/showAllCyclists";
+const timeSortUrl = "http://localhost:8080/showCyclistsSortedByTime";
 
-function fetchCyclists(){
-  return fetch(cyclistUrl).then(response => response.json()).catch(reason => alert(reason));
+sortButton.addEventListener('click', showSortedCyclists);
+randomButton.addEventListener('click', showCyclists);
+timeButton.addEventListener('click', showSortedByTime);
+
+function fetchCyclists(url){
+  return fetch(url).then(response => response.json()).catch(reason => alert(reason));
 }
 
 function emptyTable() {
@@ -13,12 +22,31 @@ function emptyTable() {
 async function showCyclists(){
   emptyTable();
 
-  const list = await fetchCyclists();
-  //console.log(list);
+  const list1 = await fetchCyclists(cyclistUrl);
+  console.log(list1);
 
-  await displayTable(list);
+  await displayTable(list1);
+}
+
+async function showSortedCyclists(){
+  emptyTable();
+
+  console.log("in sorted");
+
+  const list2 = await fetchCyclists(cyclistsSortedUrl)
+  console.log(list2);
+  await displayTable(list2)
+}
+
+async function showSortedByTime(){
+  emptyTable();
+
+  console.log("in timesort");
+
+  const list3 = await fetchCyclists(timeSortUrl);
+  console.log(list3);
+  await displayTable(list3);
 }
 
 showCyclists();
-
 
